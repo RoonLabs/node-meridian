6661de3f6b125abcc35826f2335f60a572e098c8
Danny Dulai <danny@ishiboo.com>
2017-07-29 15:51:46 -0400

    TN49

--- lib.js
+++ lib.js
@@ -7,6 +7,8 @@ let SerialPort = require("serialport"),
 function Meridian(devicetype) {
     if (devicetype == "TN51") {
         // Meridian Technical Note TN51.2
+    } else if (devicetype == "TN49") {
+        // Meridian Technical Note TN49
     } else if (devicetype == "218") {
         // Meridian 218 Zone Controller
     } else {
@@ -35,52 +37,65 @@ let _processw = function() {
                     });
 }
 
-let _tn51 = function(val, cb) {
-    this._qw.push(val + "\r");
+function tn51(val, cb) {
+    this._qw.push(val);
     _processw.call(this);
 };
 
 Meridian.prototype.volume_up = function() {
-    if (this.devicetype == "TN51") {
-        _tn51.call(this, "VP");
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
+        _tn51.call(this, "VP\r");
     } else {
-        throw new Error("device type " + this.devicetype + " do not support volume_up"); }
+        throw new Error("device type " + this.devicetype + " do not support volume_up");
+    }
 };
 Meridian.prototype.volume_down = function(val) {
-    if (this.devicetype == "TN51") {
-        _tn51.call(this, "VM");
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
+        _tn51.call(this, "VM\r");
     } else {
         throw new Error("device type " + this.devicetype + " do not support volume_down");
     }
 };
 Meridian.prototype.set_volume = function(val) {
-    if (this.devicetype == "TN51") {
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
 	if (this.properties.volume == val) return;
 	if (this.volumetimer) clearTimeout(this.volumetimer);
         this.volumetimer = setTimeout(() => {
-		_tn51.call(this, "VN" + ("00" + Number(val).toString()).slice(-2));
+            _tn51.call(this, "VN" + ("00" + Number(val).toString()).slice(-2) + "\r");
 	}, 50)
     } else {
         throw new Error("device type " + this.devicetype + " do not support set_volume");
     }
 };
 Meridian.prototype.standby = function(val) {
-    if (this.devicetype == "TN51") {
-        _tn51.call(this, "SB");
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
+        _tn51.call(this, "SB\r");
     } else {
         throw new Error("device type " + this.devicetype + " do not support standby");
     }
 };
 Meridian.prototype.set_source = function(val) {
-    if (this.devicetype == "TN51") {
-        _tn51.call(this, val.slice(0,2));
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
+        _tn51.call(this, val.slice(0,2) + "\r");
     } else {
         throw new Error("device type " + this.devicetype + " do not support set_source");
     }
 };
 Meridian.prototype.mute = function() {
-    if (this.devicetype == "TN51") {
-        _tn51.call(this, "MU");
+    if (this.devicetype == "TN51" ||
+        this.devicetype == "TN49")
+    {
+        _tn51.call(this, "MU\r");
     } else {
         throw new Error("device type " + this.devicetype + " do not support mute"); }
 };
@@ -112,80 +127,17 @@ Meridian.prototype.init = function(opts, closecb) {
 	           this.emit('volume', val);
 	       }
 
-	    } else if (/^Standby$/.test(data)) {
-	        let val = "SB";
+	    } else if (/^Standby$/.test(data) || /^\.$/.test(data)) {
+	        let val = "Standby";
 	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
 
-	    } else if (/^Muted$/.test(data)) {
-	        let val = "MU";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^CD$/.test(data)) {
-	        let val = "CD";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Aux$/.test(data)) {
-	        let val = "AX";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^SLS$/.test(data)) {
-	        let val = "MS";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^CD$/.test(data)) {
-	        let val = "CD";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Radio$/.test(data)) {
-	        let val = "RD";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^TV$/.test(data)) {
-	        let val = "TV";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Tape$/.test(data)) {
-	        let val = "TA";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Disc$/.test(data)) {
-	        let val = "DC";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Cable$/.test(data)) {
-	        let val = "CB";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Sat$/.test(data)) {
-	        let val = "SA";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^iPod$/.test(data)) {
-	        let val = "IP";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Game$/.test(data)) {
-	        let val = "GA";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^V1$/.test(data)) {
-	        let val = "V1";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^V2$/.test(data)) {
-	        let val = "V2";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^USB$/.test(data)) {
-	        let val = "US";
-	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
-
-	    } else if (/^Mixer$/.test(data)) {
-	        let val = "MX";
+	    } else if (/^Mute/.test(data)) { // Mute or Muted
+	        let val = "Muted";
 	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
 
 	    } else {
-	        console.log("[Meridian] Unknown data received:", data);
+	        let val = data;
+	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
 	    }
 
 	    if (this.initializing) {
@@ -193,6 +145,66 @@ Meridian.prototype.init = function(opts, closecb) {
 		this.emit('connected');
             }
         });
+
+    } else if (this.devicetype == "TN49") {
+        this._port = new SerialPort(opts.port, {
+            baudRate: opts.baud || 9600,
+            parser:   SerialPort.parsers.readline("\r")
+        });
+
+        this._port.on('data', data => {
+	    data = data.trim();
+	    console.log('[Meridian] received:', data);
+
+            let cmd = data.split(/  */);
+
+            let first;
+            let second;
+            if (cmd.length >= 2) {
+                second = cmd[cmd.length-1];
+                first = data.substring(0, data.length-second.length).trim();
+
+            } else {
+                second = undefined;
+                first = data;
+            }
+
+            if (second !== undefined) {
+                let vol = Number(second);
+                if (!Number.isNaN(vol)) {
+                    if (this.properties.volume != vol) {
+                        this.properties.volume = vol;
+                        this.emit('volume', vol);
+                    }
+                    return;
+                } else {
+                    if (second == "Standby") {
+                        let val = "Standby";
+                        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
+                        return;
+                    }
+                }
+            }
+
+	    if (/^Standby$/.test(first) || /^\.$/.test(first)) {
+	        let val = "Standby";
+	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
+
+	    } else if (/^Mute/.test(first)) { // Mute or Muted
+	        let val = "Muted";
+	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
+
+	    } else {
+	        let val = first;
+	        if (this.properties.source != val) { this.properties.source = val; this.emit('source', val); }
+	    }
+
+	    if (this.initializing) {
+		this.initializing = false;
+		this.emit('connected');
+            }
+        });
+
     } else {
         throw new Error("unsupported device type " + devicetype + " -- Someone forgot to write some code here.");
     }
@@ -206,9 +218,9 @@ Meridian.prototype.init = function(opts, closecb) {
     this._port.on('open', err => {
         this.emit('preconnected');
 
-        if (this.devicetype == "TN51") {
-            _tn51.call(this, this.properties.source);
-            _tn51.call(this, "VN" + ("00" + Number(this.properties.volume).toString()).slice(-2));
+        if (this.devicetype == "TN51" || this.devicetype == "TN49") {
+            _tn51.call(this, this.properties.source + "\r");
+            _tn51.call(this, "VN" + ("00" + Number(this.properties.volume).toString()).slice(-2) + "\r");
         }
     });
 
